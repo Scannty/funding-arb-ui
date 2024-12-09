96125ee7a3ee320982f3f9ab7396b5a3992ee335
Aleksa Uroseivc <aleksa.urosevic0@gmail.com>
2024-12-09 16:24:17 +0100

    refactor: Move tokens config into constants folder

---
+++ src/constants/tokens.ts
@@ -0,0 +1,23 @@
+interface TokenInfo {
+  tokenAddress: string;
+  decimals: number;
+  iconSymbol: string;
+}
+
+export const tokens: { [key: string]: TokenInfo } = {
+  BTC: {
+    tokenAddress: "0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f",
+    decimals: 8,
+    iconSymbol: "btc",
+  },
+  ETH: {
+    tokenAddress: "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
+    decimals: 18,
+    iconSymbol: "eth",
+  },
+  LINK: {
+    tokenAddress: "0xf97f4df75117a78c1A5a0DBb814Af92458539FB4",
+    decimals: 18,
+    iconSymbol: "link",
+  },
+};
