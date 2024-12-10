12b17af3a17146a7cd3e0e7410a27d255d97639e
Aleksa Uroseivc <aleksa.urosevic0@gmail.com>
2024-12-10 18:40:34 +0100

    style: Add logo and title to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,15 @@ import { ConnectKitButton } from "connectkit";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center p-4 bg-gray-200">
-      <h1 className="text-xl">Funding Arbitrage Farming</h1>
+    <header className="flex justify-between items-center p-4 shadow-md">
+      <div className="flex items-center">
+        <img
+          src="https://pbs.twimg.com/profile_images/1646991609416806408/vKLEZxhh_400x400.png"
+          alt="Hyperliquid Logo"
+          className="w-12"
+        />
+        <h1 className="ml-6 text-xl font-medium">Hyperliquid FF Strategy</h1>
+      </div>
       <ConnectKitButton />
     </header>
   );
