50c83ea9e65e07c1bb7502b88f346532b371474e
macropolo <macro_polo@yahoo.com>
2024-12-13 05:43:53 +0100

    style: add styling to header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,16 +2,21 @@ import { ConnectKitButton } from "connectkit";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center p-4 shadow-md">
-      <div className="flex items-center">
+    <header className="box-border flex flex-row flex-wrap items-center content-start justify-between p-8 gap-x-6 h-[99px] bg-white border-b border-[#D9D9D9]">
+      <div className="flex flex-row items-center p-0 gap-6 w-10 h-[35px] order-0 flex-none">
         <img
           src="https://pbs.twimg.com/profile_images/1646991609416806408/vKLEZxhh_400x400.png"
           alt="Hyperliquid Logo"
-          className="w-12"
+          className="w-[40px] h-[40px] order-0 flex-none"
         />
-        <h1 className="ml-6 text-xl font-medium">Hyperliquid FF Strategy</h1>
+        <h1 className="font-inter font-bold text-base leading-[100%] flex items-center text-[#1E1E1E] order-0 flex-none">
+          Hyperliquid FF Strategy
+        </h1>
       </div>
-      <ConnectKitButton />
+      
+      <div className="flex flex-row items-center ml-auto p-0 gap-3 w-[178px] h-8">
+        <ConnectKitButton />
+      </div>
     </header>
   );
 }
