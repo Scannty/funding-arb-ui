5919a42d28a800a2d6087210e0d229255d5743f6
macropolo <macro_polo@yahoo.com>
2024-12-17 06:03:54 +0100

    feat: Add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+// Footer.tsx
+export default function Footer() {
+    return (
+      <footer className="box-border flex flex-row flex-wrap items-start content-start gap-4 h-[99px] bg-white border-t border-[#D9D9D9]">
+        {/* Footer content will go here */}
+        {/* <div className="font-inter text-[#757575]">
+          <p>© 2024 Hyperliquid. All rights reserved.</p>
+        </div> */}
+      </footer>
+    );
+  }
